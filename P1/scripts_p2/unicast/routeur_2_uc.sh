f640489398d058c34fed154e32876f29578cf412
Hina Razanamasy <hina.razanamasy@gmail.com>
2023-02-24 04:00:50 -0500

    rename scripts P1 et fix ip mask

--- P1/scripts_p2/unicast/routeur_2_uc.sh
+++ P1/scripts_p2/unicast/routeur_2_uc.sh
@@ -2,7 +2,7 @@
 
 ip link add br0 type bridge
 ip link set dev br0 up
-ip addr add 10.1.1.2 dev eth0
+ip addr add 10.1.1.2/24 dev eth0
 ip addr show eth0
 ip link add  name vxlan10 type vxlan id 10 dev eth0 remote 10.1.1.1 local 10.1.1.2 dstport 4789
 ip addr add 20.1.1.1/24 dev vxlan10

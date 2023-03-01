a0e9db3161e1ddafc8f7ee7acf2d421b2b2a02f1
Alice LIN <alilin@e1r11p4.clusters.42paris.fr>
2023-03-01 17:09:02 +0100

    changed the host script to try to make it work in sh mode

--- P2/scripts/startHost.sh
+++ P2/scripts/startHost.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-declare -A script_mapping=(
-  ["host_alilin-1"]="/scripts_p2/host/host_1.sh"
-  ["host_alilin-2"]="/scripts_p2/host/host_2.sh"
-)
+declare -A script_mapping="host_alilin-1:/scripts_p2/host/host_1.sh host_alilin-2:/scripts_p2/host/host_2.sh"
 
 for container in $(docker ps --filter "ancestor=busybox" --format "{{.Names}}");
 do
